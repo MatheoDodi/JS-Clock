4b014ea2de60170def553eeffa6e8c642b4449c6
Matthew Dodi <matthew.dodi@gmail.com>
2018-07-06 14:25:25 -0700

    Added movement on all the hands of the clock. Also fixed a bug where the hand of Seconds would do a whole spin when it hit 60 seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 const SECOND_HAND = document.querySelector('.second-hand');
+const MIN_HAND = document.querySelector('.min-hand');
+const HOUR_HAND = document.querySelector('.hour-hand');
 
 function setDate() {
 	let now = new Date();
 	let seconds = now.getSeconds();
-	let secondsInDegrees = (seconds / 60) * 360 + 90;
+	let secondsInDegrees = ((seconds / 60) * 360) + 90;
+	let secondsNow = secondsInDegrees
+
+	if (secondsInDegrees === 90) {
+		SECOND_HAND.style.transition = 'all 0.00s'
+	}
+
 	SECOND_HAND.style.transform = `rotate(${secondsInDegrees}deg)`;
+
+	let minutes = now.getMinutes();
+	let minutesInDegrees = ((minutes / 60) * 360) + 90;
+	MIN_HAND.style.transform = `rotate(${minutesInDegrees}deg)`;
+
+	let hours = now.getHours();
+	let hoursInDegrees = ((hours / 60) * 360) + 80;
+	HOUR_HAND.style.transform = `rotate(${hoursInDegrees}deg)`;
+
 }
 
 setInterval(setDate, 1000);
